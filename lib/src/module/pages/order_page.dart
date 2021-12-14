88899489f2651aad8f38b3fc592357f8217877bd
Euclides Catunda <euclides.catunda@b2wdigital.com>
2021-12-13 22:45:22 -0300

    first commit

---
+++ lib/src/module/pages/order_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatelessWidget {
+  const OrderPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: const Text('Order Page'),
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [],
+          ),
+        ),
+      ),
+    );
+  }
+}
